fa4eb8c46a6a52bb576b98fac2a1253c6dd160f4
Marcelo Takashi <spenttie@gmail.com>
2020-02-17 00:53:40 -0300

    update side-menu

--- src/app/modules/side-nav/side-nav.component.ts
+++ src/app/modules/side-nav/side-nav.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent {
 
-  imgList:Array<Object> = [
+  imgList:Array<ImageInterface> = [
+      {src: 'assets/img/search.png', name: 'search', route: '/search', active: false },
       {src: 'assets/img/hook.png', name: 'hook', route: '/style-manual', active: false },
-      {src: 'assets/img/schedule.png', name: 'schedule', route: '/suppliers-schedule', active: false },
-      {src: 'assets/img/hook.png', name: 'computes', route: '', active: false },
+      {src: 'assets/img/calendar.png', name: 'schedule', route: '/suppliers-schedule', active: false },
   ];
 
-  constructor() { }
+  constructor(private router: Router) { 
+    router.events.subscribe( (event) => {
+      if(event instanceof NavigationEnd) {
+        this,this.findRoute(event.url);
+      }
+    });
+  }
 
-  ngOnInit() {
+  findRoute(routeName: String) {
+    const route = this.imgList.find( (img) => {
+      return img.route === routeName;
+    });
+    route.active = true;
+    this.setImgList(route);
+  }
+
+  setImgList(route: ImageInterface) {
+    this.imgList.filter( (item, index) => {
+      if(item.name === route.name) {
+        this.imgList.splice(index, 1);
+        this.imgList.unshift(route);
+      }
+    });
   }
 
 }
