e9b6acd62229416c6831f1998b7da6a9d5e8b2f3
Takashi <Spenttie@gmail.com>
2020-02-27 13:26:50 -0300

    rounabou for heroku

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 import { environment } from '../environments/environment';
 
-export const API_BASE = environment.api;
+// export const API_BASE = environment.api;
+export const API_BASE = "http://rare-piece-api.herokuapp.com/api/v1"
