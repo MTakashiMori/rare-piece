5227315db560db1d08331a678501ba546e6f7a6d
Marcelo Takashi <32560710+MTakashiMori@users.noreply.github.com>
2020-02-27 13:46:52 -0300

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,4 @@
 import { environment } from '../environments/environment';
 
 // export const API_BASE = environment.api;
-export const API_BASE = "http://rare-piece-api.herokuapp.com/api/v1"
+export const API_BASE = "https://rare-piece-api.herokuapp.com/api/v1"
